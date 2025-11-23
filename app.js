51f8dadce3204be284844d1d5d425edb18ecac4e
garkan1297 <147925008+garkan1297@users.noreply.github.com>
2025-11-24 00:24:12 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-23T21:23:56.010Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-23T21:24:12.027Z
+Commit: Resolve timezone issues in date handling
